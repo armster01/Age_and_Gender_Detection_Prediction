5c723b2b11e45f46474377f02e436dd152560127
ADITTYA SAHA <armstersaha8@gmail.com>
2024-09-28 21:28:45 +0530

    Create lebels.py

---
+++ lebels.py
@@ -0,0 +1,7 @@
+print(f"Number of age_labels: {len(age_labels)}")
+print(f"Number of gender_labels: {len(gender_labels)}")
+print(f"Number of image_paths: {len(image_paths)}")
+
+Number of age_labels: 23708
+Number of gender_labels: 23708
+Number of image_paths: 23708
